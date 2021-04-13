e4a3f4b71c4155d612bde1c1102430595288a086
Hong-She Liang <sorn@taorules.com>
2021-04-13 22:08:36 +0800

    Drop python 2.x support
    And upgrade to python 3.8 supports

--- setup.py
+++ setup.py
@@ -10,17 +10,17 @@ except:
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file, open('HISTORY.rst') as history_file:
-    long_description = (readme_file.read() + "\n\n" + history_file.read())
+with open("README.rst") as readme_file, open("HISTORY.rst") as history_file:
+    long_description = readme_file.read() + "\n\n" + history_file.read()
 
 tushare_requires = [
     # Because tushare library does not setup correct library dependences, so
     # we have to add library dependences here for complete the library search
     # behavior.
-    'lxml',
-    'pandas',
-    'beautifulsoup4',
-    'requests',
+    "lxml",
+    "pandas",
+    "beautifulsoup4",
+    "requests",
 ]
 
 # What the hell is tushare doing?? Reference itself in setup.py ??
@@ -29,43 +29,44 @@ for require in tushare_requires:
     pipmain(["install", require])
 
 install_requires = [
-    'tushare',
+    "tushare",
 ]
 
 setup_requires = [
-    'pytest-runner',
+    "pytest-runner",
 ]
 
 tests_requires = [
-    'pytest',
-    'click',
+    "pytest",
+    "click",
 ]
 
 setup(
-    name='tushare-deps',
-    version='0.0.5',
+    name="tushare-deps",
+    version="0.0.5",
     description="A library prepare the dependences for tushare library",
     long_description=long_description,
     author="Hong-She Liang",
-    author_email='starofrainnight@gmail.com',
-    url='https://github.com/starofrainnight/tushare-deps',
+    author_email="starofrainnight@gmail.com",
+    url="https://github.com/starofrainnight/tushare-deps",
     packages=find_packages(),
     include_package_data=True,
     install_requires=install_requires,
     license="Apache Software License",
     zip_safe=False,
-    keywords='tushare',
+    keywords="tushare",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    test_suite='tests',
+    test_suite="tests",
     tests_require=tests_requires,
     setup_requires=setup_requires,
 )
